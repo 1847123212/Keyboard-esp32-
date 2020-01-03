b7f0d4fcedaa1004c55921686fd45b09e03c8cb8
xxzhang <xuanxuan.zhang@ingenic.com>
2020-01-03 09:37:12 +0800

    修改includepath减少在vscode显示没必要的波浪线

--- main/ble_hidd_demo_main.c
+++ main/ble_hidd_demo_main.c
@@ -168,7 +168,7 @@ void hid_demo_task(void *pvParameters)
             // ESP_LOGI(HID_DEMO_TAG, "BT Connecting success");
             // send_volum_up = true;
             // //uint8_t key_vaule = {HID_KEY_A};
-            esp_hidd_send_keyboard_value(hid_conn_id, 0, &key_vaule, 1);
+            // esp_hidd_send_keyboard_value(hid_conn_id, 0, &key_vaule, 1);
             // esp_hidd_send_consumer_value(hid_conn_id, HID_CONSUMER_VOLUME_UP, true);
             // vTaskDelay(3000 / portTICK_PERIOD_MS);
             // if (send_volum_up) {
